7b3a4a6495ea34d79620666624a03ce2e2fba5a7
SIRO\gmlwo <gmlwo990518@naver.com>
2025-02-04 16:57:44 +0900

    Test

--- BaaarkingDog/0X03/10808/소스1.cpp
+++ BaaarkingDog/0X03/10808/소스1.cpp
@@ -1,1 +1,22 @@
-#TestSDASD
+#include <iostream>
+
+using namespace std;
+
+int func1(int arr[], int n)
+{
+	int cnt = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] % 5 == 0) cnt++;
+	}
+	return cnt;
+}
+
+int main() 
+{
+	int arr[], n;
+	cin >> arr[], n;
+
+	cout << func(arr[], n) << endl;
+	return 0;
+}
